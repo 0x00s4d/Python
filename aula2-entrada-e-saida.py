d7fd5f828958e1a53b76d560fd99ab0128c5b9d7
0x00s4d <35602664+0x00s4d@users.noreply.github.com>
2018-01-19 19:40:16 -0200

    Update aula2-entrada-e-saida.py

--- aula2-entrada-e-saida.py
+++ aula2-entrada-e-saida.py
@@ -1,9 +1,13 @@
 nome = 'Renato'
 idade = 20
+altura = 1.79
 tipo_nome = type(nome)
 tipo_idade = type(idade)
+tipo_altura = type(altura)
 
 print(nome)
 print(idade)
+print(altura)
 print(tipo_nome)
 print(tipo_idade)
+print(tipo_altura)
